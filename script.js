6e127bd90638c1f0c3aece39cc3012bd289c4aac
gabriel_saucedo <saucedogabriel99@gmail.com>
2024-12-17 19:50:44 -0300

    [FEAT] Logic structured

--- script.js
+++ script.js
@@ -1,28 +1,53 @@
-let humanScore  = 0;
+let humanScore = 0;
 let computerScore  = 0;
 
 const getHumanChoice = () => {
     let value = prompt("Please, Enter a number of 1 to 3");
-    return value;
+
+    if (value == 1) {
+        humanChoice = '🪨';
+    } else if (value == 2) {
+        humanChoice = '🧻';
+    } else if (value == 3) {
+        humanChoice = '✂️';
+    } else {
+        alert('Wrong number, please follow the instructions.')
+    }
     
+    return value;
 }
 
 const getComputerChoice = () => {
     let number = Math.floor(Math.random() * (4 - 1) + 1);
-    let computerChoice = '';
+    return number;
+}
 
-    if (number == 1) {
-        computerChoice = '🪨';
-    } else if (number == 2) {
-        computerChoice = '🧻';
-    } else if (number == 3) {
-        computerChoice = '✂️';
+const playRound = (resultHuman, resultComputer) => {
+    console.log(`Human result: ${resultHuman} \n Computer result: ${resultComputer}`);
+
+    if (resultHuman == resultComputer) {
+        console.log('Its a draw!');
+    }else if ((resultHuman == (resultComputer + 1)) || (resultHuman == 1 && resultComputer == 3)) {
+        console.log('Human wins!');
+        humanScore++;
+    } else if ((resultComputer == (resultHuman + 1)) || (resultComputer == 1 && resultHuman == 3)){
+        console.log('Computer wins!');
+        computerScore++;
     }
-    
-    return computerChoice;
 }
 
-const playRound = (getHumanChoice, getComputerChoice) => {
-    const getHumanChoice = humanChoiceFunc();
-    const getComputerChoice = computerChoiceFunc();
+for (let i = 0; i < 5; i++) {
+    resultHuman = getHumanChoice();
+    resultComputer = getComputerChoice();
+    playRound(resultHuman, resultComputer);
 }
+
+if (humanScore == computerScore){
+    console.log('Its a final draw! 🤝');
+}else if(humanScore > computerScore){
+    console.log('Humans won the game! 😁');
+} else {
+    console.log('Computers won the game!💻');
+}
+
+console.log(`Human final result: ${humanScore} \n Computer final result: ${computerScore}`);
